2ef0b8cbc7bbc7092adb7b2e5aaf81b8c75aa943
codeswindler <wilsonliam425@gmail.com>
2026-04-13 01:25:20 +0300

    Zoom out mobile viewport

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,14 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
   title: "AirPulse Airtime Service",
   description: "Advanced USSD Airtime Dashboard",
+};
+
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 0.8,
 };
 
 export default function RootLayout({
